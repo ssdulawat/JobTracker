5188cab1211282aa33009ee2371a34e1044df766
Shakti <shakti5385@gmail.com>
2020-05-30 14:00:26 +0530

    Added Main JT MDI Form

---
+++ JobTracker/JobTracker/JobTrackingMDIForm/JobAndTrackingMDI.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace JobTracker.JobTrackingMDIForm
+{
+    partial class JobAndTrackingMDI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.calendarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.managerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addTImeExpenseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contactToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dbaseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.jTListItemToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.invoiceToolsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lbkApprovedVersion = new System.Windows.Forms.LinkLabel();
+            this.btnCloasAll = new System.Windows.Forms.Button();
+            this.lblLogin = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.calendarToolStripMenuItem,
+            this.managerToolStripMenuItem,
+            this.addTImeExpenseToolStripMenuItem,
+            this.contactToolStripMenuItem,
+            this.dbaseToolStripMenuItem,
+            this.jTListItemToolStripMenuItem,
+            this.toolsToolStripMenuItem,
+            this.invoiceToolsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1130, 28);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // calendarToolStripMenuItem
+            // 
+            this.calendarToolStripMenuItem.Name = "calendarToolStripMenuItem";
+            this.calendarToolStripMenuItem.Size = new System.Drawing.Size(82, 26);
+            this.calendarToolStripMenuItem.Text = "Calendar";
+            this.calendarToolStripMenuItem.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // managerToolStripMenuItem
+            // 
+            this.managerToolStripMenuItem.Name = "managerToolStripMenuItem";
+            this.managerToolStripMenuItem.Size = new System.Drawing.Size(82, 26);
+            this.managerToolStripMenuItem.Text = "Manager";
+            // 
+            // addTImeExpenseToolStripMenuItem
+            // 
+            this.addTImeExpenseToolStripMenuItem.Name = "addTImeExpenseToolStripMenuItem";
+            this.addTImeExpenseToolStripMenuItem.Size = new System.Drawing.Size(148, 26);
+            this.addTImeExpenseToolStripMenuItem.Text = "Add Time/Expense";
+            // 
+            // contactToolStripMenuItem
+            // 
+            this.contactToolStripMenuItem.Name = "contactToolStripMenuItem";
+            this.contactToolStripMenuItem.Size = new System.Drawing.Size(80, 26);
+            this.contactToolStripMenuItem.Text = "Contacts";
+            // 
+            // dbaseToolStripMenuItem
+            // 
+            this.dbaseToolStripMenuItem.Name = "dbaseToolStripMenuItem";
+            this.dbaseToolStripMenuItem.Size = new System.Drawing.Size(65, 26);
+            this.dbaseToolStripMenuItem.Text = "Dbase";
+            // 
+            // jTListItemToolStripMenuItem
+            // 
+            this.jTListItemToolStripMenuItem.Name = "jTListItemToolStripMenuItem";
+            this.jTListItemToolStripMenuItem.Size = new System.Drawing.Size(96, 26);
+            this.jTListItemToolStripMenuItem.Text = "JT List Item";
+            // 
+            // toolsToolStripMenuItem
+            // 
+            this.toolsToolStripMenuItem.Name = "toolsToolStripMenuItem";
+            this.toolsToolStripMenuItem.Size = new System.Drawing.Size(58, 26);
+            this.toolsToolStripMenuItem.Text = "Tools";
+            // 
+            // invoiceToolsToolStripMenuItem
+            // 
+            this.invoiceToolsToolStripMenuItem.Name = "invoiceToolsToolStripMenuItem";
+            this.invoiceToolsToolStripMenuItem.Size = new System.Drawing.Size(109, 26);
+            this.invoiceToolsToolStripMenuItem.Text = "Invoice Tools";
+            // 
+            // lbkApprovedVersion
+            // 
+            this.lbkApprovedVersion.AutoSize = true;
+            this.lbkApprovedVersion.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.lbkApprovedVersion.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbkApprovedVersion.ForeColor = System.Drawing.Color.Maroon;
+            this.lbkApprovedVersion.LinkColor = System.Drawing.Color.Maroon;
+            this.lbkApprovedVersion.Location = new System.Drawing.Point(810, 9);
+            this.lbkApprovedVersion.Name = "lbkApprovedVersion";
+            this.lbkApprovedVersion.Size = new System.Drawing.Size(119, 18);
+            this.lbkApprovedVersion.TabIndex = 3;
+            this.lbkApprovedVersion.TabStop = true;
+            this.lbkApprovedVersion.Text = "Approved Version";
+            // 
+            // btnCloasAll
+            // 
+            this.btnCloasAll.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnCloasAll.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCloasAll.Font = new System.Drawing.Font("Calibri", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCloasAll.ForeColor = System.Drawing.Color.OrangeRed;
+            this.btnCloasAll.Location = new System.Drawing.Point(966, 4);
+            this.btnCloasAll.Name = "btnCloasAll";
+            this.btnCloasAll.Size = new System.Drawing.Size(75, 23);
+            this.btnCloasAll.TabIndex = 4;
+            this.btnCloasAll.Text = "Close All";
+            this.btnCloasAll.UseVisualStyleBackColor = false;
+            // 
+            // lblLogin
+            // 
+            this.lblLogin.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.lblLogin.FlatAppearance.BorderColor = System.Drawing.Color.Blue;
+            this.lblLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblLogin.Font = new System.Drawing.Font("Calibri", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLogin.Location = new System.Drawing.Point(1047, 4);
+            this.lblLogin.Name = "lblLogin";
+            this.lblLogin.Size = new System.Drawing.Size(76, 23);
+            this.lblLogin.TabIndex = 9;
+            this.lblLogin.Text = "Admin Login";
+            this.lblLogin.UseMnemonic = false;
+            this.lblLogin.UseVisualStyleBackColor = false;
+            // 
+            // JobAndTrackingMDI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.ClientSize = new System.Drawing.Size(1130, 450);
+            this.Controls.Add(this.lblLogin);
+            this.Controls.Add(this.btnCloasAll);
+            this.Controls.Add(this.lbkApprovedVersion);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "JobAndTrackingMDI";
+            this.Text = "JobAndTrackingMDI";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem calendarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem managerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addTImeExpenseToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem contactToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dbaseToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem jTListItemToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem invoiceToolsToolStripMenuItem;
+        private System.Windows.Forms.LinkLabel lbkApprovedVersion;
+        private System.Windows.Forms.Button btnCloasAll;
+        public System.Windows.Forms.Button lblLogin;
+    }
+}
