1564f91aa12a2da74a5baecbc94d716c66d6bec0
Shakti <shakti5385@gmail.com>
2020-05-30 20:04:41 +0530

    Added bottom Panel in MDI Form.

--- JobTracker/JobTracker/JobTrackingMDIForm/JobAndTrackingMDI.Designer.cs
+++ JobTracker/JobTracker/JobTrackingMDIForm/JobAndTrackingMDI.Designer.cs
@@ -40,12 +40,21 @@
             this.lbkApprovedVersion = new System.Windows.Forms.LinkLabel();
             this.btnCloasAll = new System.Windows.Forms.Button();
             this.lblLogin = new System.Windows.Forms.Button();
+            this.kryptonPanel1 = new ComponentFactory.Krypton.Toolkit.KryptonPanel();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.lblVersion = new System.Windows.Forms.Label();
+            this.lnlLblNewVersion = new System.Windows.Forms.LinkLabel();
+            this.linklbl = new System.Windows.Forms.LinkLabel();
             this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonPanel1)).BeginInit();
+            this.kryptonPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
             // 
             this.menuStrip1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.menuStrip1.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.calendarToolStripMenuItem,
@@ -58,7 +67,7 @@
             this.invoiceToolsToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1130, 28);
+            this.menuStrip1.Size = new System.Drawing.Size(1130, 30);
             this.menuStrip1.TabIndex = 1;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -152,12 +161,90 @@
             this.lblLogin.UseMnemonic = false;
             this.lblLogin.UseVisualStyleBackColor = false;
             // 
+            // kryptonPanel1
+            // 
+            this.kryptonPanel1.Controls.Add(this.linklbl);
+            this.kryptonPanel1.Controls.Add(this.lnlLblNewVersion);
+            this.kryptonPanel1.Controls.Add(this.lblVersion);
+            this.kryptonPanel1.Controls.Add(this.lblTime);
+            this.kryptonPanel1.Controls.Add(this.lblDate);
+            this.kryptonPanel1.Location = new System.Drawing.Point(0, 417);
+            this.kryptonPanel1.Name = "kryptonPanel1";
+            this.kryptonPanel1.Size = new System.Drawing.Size(1130, 31);
+            this.kryptonPanel1.TabIndex = 11;
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.BackColor = System.Drawing.Color.Transparent;
+            this.lblDate.Font = new System.Drawing.Font("Calibri", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDate.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.lblDate.Location = new System.Drawing.Point(36, 7);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(36, 17);
+            this.lblDate.TabIndex = 0;
+            this.lblDate.Text = "Date";
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.BackColor = System.Drawing.Color.Transparent;
+            this.lblTime.Font = new System.Drawing.Font("Calibri", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTime.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.lblTime.Location = new System.Drawing.Point(235, 7);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(36, 17);
+            this.lblTime.TabIndex = 1;
+            this.lblTime.Text = "Time";
+            // 
+            // lblVersion
+            // 
+            this.lblVersion.AutoSize = true;
+            this.lblVersion.BackColor = System.Drawing.Color.Transparent;
+            this.lblVersion.Enabled = false;
+            this.lblVersion.Font = new System.Drawing.Font("Calibri", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblVersion.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.lblVersion.Location = new System.Drawing.Point(430, 7);
+            this.lblVersion.Name = "lblVersion";
+            this.lblVersion.Size = new System.Drawing.Size(52, 17);
+            this.lblVersion.TabIndex = 2;
+            this.lblVersion.Text = "Version";
+            // 
+            // lnlLblNewVersion
+            // 
+            this.lnlLblNewVersion.AutoSize = true;
+            this.lnlLblNewVersion.BackColor = System.Drawing.Color.Transparent;
+            this.lnlLblNewVersion.DisabledLinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(74)))), ((int)(((byte)(116)))));
+            this.lnlLblNewVersion.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lnlLblNewVersion.ForeColor = System.Drawing.Color.Maroon;
+            this.lnlLblNewVersion.LinkColor = System.Drawing.Color.Maroon;
+            this.lnlLblNewVersion.Location = new System.Drawing.Point(592, 7);
+            this.lnlLblNewVersion.Name = "lnlLblNewVersion";
+            this.lnlLblNewVersion.Size = new System.Drawing.Size(124, 18);
+            this.lnlLblNewVersion.TabIndex = 3;
+            this.lnlLblNewVersion.TabStop = true;
+            this.lnlLblNewVersion.Text = "Check new version";
+            // 
+            // linklbl
+            // 
+            this.linklbl.AutoSize = true;
+            this.linklbl.BackColor = System.Drawing.Color.Transparent;
+            this.linklbl.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linklbl.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.linklbl.Location = new System.Drawing.Point(1001, 7);
+            this.linklbl.Name = "linklbl";
+            this.linklbl.Size = new System.Drawing.Size(115, 18);
+            this.linklbl.TabIndex = 4;
+            this.linklbl.TabStop = true;
+            this.linklbl.Text = "www.valjato.com";
+            // 
             // JobAndTrackingMDI
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.HotTrack;
             this.ClientSize = new System.Drawing.Size(1130, 450);
+            this.Controls.Add(this.kryptonPanel1);
             this.Controls.Add(this.lblLogin);
             this.Controls.Add(this.btnCloasAll);
             this.Controls.Add(this.lbkApprovedVersion);
@@ -168,6 +255,9 @@
             this.Text = "JobAndTrackingMDI";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonPanel1)).EndInit();
+            this.kryptonPanel1.ResumeLayout(false);
+            this.kryptonPanel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -187,5 +277,11 @@
         private System.Windows.Forms.LinkLabel lbkApprovedVersion;
         private System.Windows.Forms.Button btnCloasAll;
         public System.Windows.Forms.Button lblLogin;
+        private ComponentFactory.Krypton.Toolkit.KryptonPanel kryptonPanel1;
+        private System.Windows.Forms.LinkLabel linklbl;
+        private System.Windows.Forms.LinkLabel lnlLblNewVersion;
+        private System.Windows.Forms.Label lblVersion;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Label lblDate;
     }
 }
